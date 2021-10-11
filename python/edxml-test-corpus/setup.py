58772948e5e62686afc899c8102652bbf6fbd728
Dik Takken <dik.takken@edxml.org>
2021-10-11 15:42:49 +0200

    Fix incorrect e-mail address

--- python/edxml-test-corpus/setup.py
+++ python/edxml-test-corpus/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     name="edxml-test-corpus",
     version="3.0.0.dev2",
     author="Dik Takken",
-    author_email="d.h.j.takken@edxml.org",
+    author_email="dik.takken@edxml.org",
     description="A collection of portable unit tests for EDXML implementations",
     long_description=long_description,
     long_description_content_type="text/x-rst",
